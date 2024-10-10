4208911fab56882e8725ef06b77553e4e48cd60e
Joerozvicariato <joerozvicariato287@gmail.com>
2024-10-10 22:25:11 +0800

    Added addmarker'

--- applet-2.js
+++ applet-2.js
@@ -4,4 +4,9 @@ class LeafletMap {
         this.map = L.map(containerId).setView(center, zoom);
         this.initTileLayer();
     }
+    addMarker(lat, lng, message) {
+        const marker = L.marker([lat, lng]).addTo(this.map);
+        marker.bindPopup(message);
+    }
+
 }
